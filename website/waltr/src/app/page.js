ad2c8fae455b2e73100fc93c18bbc2e6c97acd22
jchernin4 <jeremychernin@gmail.com>
2026-02-27 17:15:46 -0500

    feat: added ocr endpoint

--- website/waltr/src/app/page.js
+++ website/waltr/src/app/page.js
@@ -1,11 +1,25 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <p>Testing</p>
-      </main>
-    </div>
-  );
+	useEffect(() => {
+		fetch("/api/ocr", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				text: "2x + 3y = 5"
+			})
+		});
+	}, []);
+
+	return (
+		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+			<main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+				<p>Testing</p>
+			</main>
+		</div>
+	);
 }
