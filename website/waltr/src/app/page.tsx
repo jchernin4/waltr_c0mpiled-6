2a4e001e2a567faecf2c2119fa661601820daef2
jchernin4 <jeremychernin@gmail.com>
2026-02-27 17:34:20 -0500

    feat: layout setup

--- website/waltr/src/app/page.tsx
+++ website/waltr/src/app/page.tsx
@@ -18,18 +18,27 @@ export default function Home() {
 	}, []);
 
 	return (
-		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-			<main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-				<p>Testing</p>
+		<div className="grid h-screen w-screen grid-cols-[1fr_auto] grid-rows-[1fr_auto] bg-white dark:bg-black">
+			<main className="col-start-1 row-start-1">
 				<ReactSketchCanvas
 					allowOnlyPointerType="all"
-					width="70%"
-					height="800px"
+					width="100%"
+					height="100%"
 					canvasColor="transparent"
 					strokeColor="#023423"
-					style={{ touchAction: "none" }}     // key: apply directly here too
+					style={{ touchAction: "none" }}
 				/>
 			</main>
+
+			{/* Preview area*/}
+			<aside className="col-start-2 row-start-1 w-64 bg-zinc-200 dark:bg-zinc-800">
+				<p>Right</p>
+			</aside>
+
+			{/* LLM Response */}
+			<footer className="col-span-2 row-start-2 h-48 bg-zinc-100 dark:bg-zinc-900">
+				<p>Bottom</p>
+			</footer>
 		</div>
 	);
 }
