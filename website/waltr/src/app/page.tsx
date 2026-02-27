593a520fdc1d4c178d893dbed6a48e4f464b118b
jchernin4 <jeremychernin@gmail.com>
2026-02-27 17:27:59 -0500

    feat: added canvas + layout changes

--- website/waltr/src/app/page.tsx
+++ website/waltr/src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { ReactSketchCanvas } from "react-sketch-canvas";
 import { useEffect } from "react";
+import React from "react";
 
 export default function Home() {
 	useEffect(() => {
@@ -19,6 +21,14 @@ export default function Home() {
 		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
 			<main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
 				<p>Testing</p>
+				<ReactSketchCanvas
+					allowOnlyPointerType="all"
+					width="70%"
+					height="800px"
+					canvasColor="transparent"
+					strokeColor="#023423"
+					style={{ touchAction: "none" }}     // key: apply directly here too
+				/>
 			</main>
 		</div>
 	);
